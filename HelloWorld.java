40298517e84e9ed4b207080b036aaee7f767c9d5
Michael B. Chun <chun_michael@ymail.com>
2014-12-16 02:40:59 -0800

    Java Implementation
    Obligatory Hello World program.

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//The first program most people write.
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	//Prints out the string 'Hello, World" followed by a newline.
+        System.out.println("Hello, World");
+    }
+
+}
